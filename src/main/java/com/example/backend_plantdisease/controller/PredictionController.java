20401ea28d0c143ba9cfd03a864afc83ba524c6f
Jayaprakash2006 <senthillakshmi1974@gmail.com>
2025-12-24 14:46:19 +0530

    Update Gemini Conn

--- src/main/java/com/example/backend_plantdisease/controller/PredictionController.java
+++ src/main/java/com/example/backend_plantdisease/controller/PredictionController.java
@@ -3,6 +3,7 @@ package com.example.backend_plantdisease.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import java.util.Map;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import com.example.backend_plantdisease.service.PredictionService;
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class PredictionController {
 
     @Autowired
